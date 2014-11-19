9869833153b701af313ed3c1876bd6ce56153273
Craig Booth <craigmbooth@gmail.com>
2014-11-19 09:56:23 -0600

    Changed docstrings in data_munging.py

--- data_munging.py
+++ data_munging.py
@@ -1,5 +1,9 @@
 def convert(input):
-    """"""
+    """This function takes some sort of dictionary structure and changes unicode
+    strings into plain bytestrings.  This is kinda horrible, but it's needed for
+    mincemeat.py not to freak out.
+    """
+
     if isinstance(input, dict):
         return {convert(key): convert(value) for
                 key, value in input.iteritems()}
@@ -13,7 +17,7 @@ def convert(input):
 def munge_data(data, initial_pr):
     """Our MapReduce code expects the keys and values to be of the form
 
-    ``(user, pagerank, n_friends): [list, of, friends]``
+    ``(user, pagerank): [list, of, friends]``
 
     So we need to attach an initial guess at the pagerank to each of our
     nodes."""
